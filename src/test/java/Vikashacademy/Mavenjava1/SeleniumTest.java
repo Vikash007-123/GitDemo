0812d0aec488242d6a6518091e3aeebae392202b
vikash <vikashprogrammer123@gmail.com>
2020-10-14 12:42:44 +0530

    change from vikash side

--- src/test/java/Vikashacademy/Mavenjava1/SeleniumTest.java
+++ src/test/java/Vikashacademy/Mavenjava1/SeleniumTest.java
@@ -7,6 +7,8 @@ public class SeleniumTest {
 	@Test
 	public void check() {
 		System.out.println("check selenium");
+                System.out.println("check cucumber");
+
 	}
 	@Test
 	public void uiScreen() {
