9b65c4c1c2b66db6170ec577786dd3c66a47d2f3
vikash <vikashprogrammer123@gmail.com>
2020-10-14 11:41:19 +0530

    my first commit

---
+++ src/test/java/Vikashacademy/Mavenjava1/SeleniumTest.java
@@ -0,0 +1,20 @@
+package Vikashacademy.Mavenjava1;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void check() {
+		System.out.println("check selenium");
+	}
+	@Test
+	public void uiScreen() {
+		System.out.println("Here I Test UI Screen");
+	}
+	@Test
+	public void login() {
+		System.out.println("login method");
+	}
+
+}
